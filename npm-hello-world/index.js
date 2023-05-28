84885ac504394831a06ca214a6c1696d50c5a961
Denis Arkhireev <den-uln@mail.ru>
2023-05-29 01:42:20 +0400

    Installed prettier

--- npm-hello-world/index.js
+++ npm-hello-world/index.js
@@ -1,4 +1,4 @@
-import _ from 'lodash';
+import _ from "lodash";
 
-console.log('Hello from index.js!');
-console.log(_.last(['one', 'two']));
+console.log("Hello from index.js!");
+console.log(_.last(["one", "two"]));
